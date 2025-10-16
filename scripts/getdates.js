7b5c86919732dbd0f9a11c365cfb129db7283f46
Innocent Nkuah <innocentnkuah2@gmail.com>
2025-10-16 12:50:40 +0300

    ghana

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,3 @@
-  <script src="/fiveserver.js"></script>
+let d = new Date();
+document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()}`;
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
